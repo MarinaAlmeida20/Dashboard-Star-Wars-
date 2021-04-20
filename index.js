61611d72bbb98e7864aef25a6712b52fd4262f35
Marina <marinabrito.ma@gmail.com>
2021-04-20 22:02:04 +0100

    created and filled the table

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const planetsCount = document.getElementById("planets");
 const spaceshipCount = document.getElementById("spaceship");
 
 fillCount();
+fillTable()
 
 function fillCount(){
     Promise.all([swapiGet('people/'),
@@ -11,12 +12,28 @@ function fillCount(){
                  swapiGet('planets/'),
                  swapiGet('starships/')
                 ]).then(function (results){
-        console.log(results)
+        // console.log(results)
         characterCount.innerHTML = results[0].data.count;
         moonsCount.innerHTML = results[1].data.count;
         planetsCount.innerHTML = results[2].data.count;
         spaceshipCount.innerHTML = results[3].data.count;
     })
+}
+
+async function fillTable() {
+    const response = await swapiGet('films/');
+    const tableData = response.data.results;
+    console.log(tableData);
+    tableData.forEach(film => {
+        $("#filmsTable").append(
+            `<tr>
+            <td>${film.title}</td>
+            <td>${moment(film.release_date).format('DD/MM/YYYY')}</td>
+            <td>${film.director}</td>
+            <td>${film.episode_id}</td>
+            </tr>`);
+    })
+    
 }
 
 function swapiGet(param) {
